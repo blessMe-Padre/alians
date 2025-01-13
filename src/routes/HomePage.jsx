13dc6b3d378d7bf513c85daa26ac926d45fcc368
Сергей Субботин <zarodiny@yandex.ru>
2025-01-14 09:53:18 +1000

    feat" добавляет секцию hero

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,6 +1,9 @@
+import styles from "../styles/homepage.module.css";
+
 import { Helmet } from "react-helmet";
 
 import { MapInfo } from "../sections";
+import Button from "./../components/Button/Button";
 
 export default function HomePage() {
     return (
@@ -29,10 +32,41 @@ export default function HomePage() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Helmet>
 
-            <div>HomePage</div>
+            <section className={`${styles.hero} hero`}>
+                <div className={styles.hero_bg_wrapper}>
+                    <img className={styles.hero_bg} src="./hero.webp" alt="hero" width={1800} height={751} />
+
+                    <div className={styles.hero_content}>
+                        <p className={styles.hero_text}>Железная надежность, железная выгода...</p>
+                        <h1 className={styles.hero_title}>Переработка лома черных и цветных металлов</h1>
+                        <Button
+                            href="#popup"
+                            text="Получить оценку металлолома"
+                        />
+                    </div>
+                </div>
+
+                <div className="container">
+                    <ul className={styles.hero_list}>
+                        <li>
+                            <img src="./icons/hero-icon-1.svg" alt="icon" width={55} height={55} />
+                            <p>Широкий спектр услуг:приемка лома, демонтаж и разделка, погрузка и перевозка собственными силами
+                            </p>                </li>
+                        <li>
+                            <img src="./icons/hero-icon-2.svg" alt="icon" width={55} height={55} />
+                            <p>Надежная репутация на Дальнем Востоке более 10 лет. Работает 5 приемных пунктов</p>                </li>
+                        <li>
+                            <img src="./icons/hero-icon-3.svg" alt="icon" width={55} height={55} />
+                            <p>Наши участки оснащены современными качественным оборудованием</p>                </li>
+                        <li>
+                            <img src="./icons/hero-icon-4.svg" alt="icon" width={55} height={55} />
+                            <p>Высокие цены, быстрая оплата. Используем индивидуальный подход к каждому клиенту</p>                </li>
+                    </ul>
+                </div>
+            </section>
 
 
-            <MapInfo pattern={2} />
+            {/* <MapInfo pattern={2} /> */}
 
         </>
     )
