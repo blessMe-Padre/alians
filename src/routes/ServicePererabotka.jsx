07f46d6accc4b78991650080b337b0c97c24b856
kkirrin <khlebnikov-2016@mail.ru>
2025-01-14 16:40:09 +1000

    feat: страница Услуги Переработки

--- src/routes/ServicePererabotka.jsx
+++ src/routes/ServicePererabotka.jsx
@@ -1,5 +1,15 @@
 import { Helmet } from "react-helmet";
 
+import styles from '../styles/service_pererabotka.module.css';
+
+import pattern from '/service/pattern.png';
+import mettals_1 from '/service/mettals_1.png';
+import mettals_2 from '/service/mettals_2.png';
+
+import per_1 from '/service/per_1.svg';
+import per_2 from '/service/per_2.svg';
+import per_3 from '/service/per_3.svg';
+
 import { ServiceHero } from "../components/index";
 export default function ServicePererabotka() {
     return (
@@ -33,6 +43,44 @@ export default function ServicePererabotka() {
                 image="/service/service_1.png"
                 desc="Используем комплексный подход по взаимодействию с клиентами. Применяем современные технологии и высокий уровень профессионализма наших сотрудников для улучшения качества процесса приемки и формирования лояльности клиентов."
             />
+
+            <section className={styles.section}>
+                <div className="page-wrapper">
+                    <h2 className="title toggle-color center">Комплексная переработка <br /> металлолома</h2>
+
+                    <div className={styles.pererabotka_wrapper}>
+                        
+                        <div>
+                            <img src={pattern} className={styles.pattern} alt="pattern" />
+                        </div>
+
+                        <div className={styles.pererabotka_wrapper_content}>
+                            <ul className={styles.pererabotka_list}>
+                                <li className={styles.pererabotka_item}>
+                                    <img src={per_1} alt="" className="" />
+                                    <p className={styles.item_desc}>
+                                        Комплексная переработка металлолома способствует рациональному использованию природных ресурсов и минимизации объемов отходов. Мы перерабатываем лом и отправляем его на экспорт и на заводы нашей страны.
+                                    </p>
+                                </li>
+
+                                <li className={styles.pererabotka_item}>
+                                    <img src={per_1} alt="" className="" />
+                                    <p className={styles.item_desc}>
+                                        Процесс переработки лома включает в себя сортировку, измельчение, очистку с последующим прессованием его в брикеты. Недостаточная эффективность технологий, нарушение требований к переработке – это одна из проблем, стоящих перед компаниями. На нашем предприятии используется современное оборудование для сортировки лома, у сотрудников, осуществляющих приемку, имеются все разрешительные документы для безопасной переработки лома.   
+                                    </p>
+                                </li>
+
+                                <li className={styles.pererabotka_item}>
+                                    <img src={per_1} alt="" className="" />
+                                    <p className={styles.item_desc}>
+                                        Мы предоставляем Вам контейнер с открытым верхом под накопление лома и отходов. В процессе осуществления деятельности все металлические отходы будут складироваться в него. После наполнения (15-20 тонн), мы привозим порожний и забираем наполненный контейнер. Автомобиль самостоятельно выгружает и загружает контейнеры на прицеп, тем самым экономя Ваше время и деньги. 
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
         </>
     )
